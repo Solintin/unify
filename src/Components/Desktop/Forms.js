7bb568fe3758163afa4b9923b6024bfdcffa02cf
DevSaalih <soliualley@gmail.com>
2022-05-10 10:44:36 +0100

    Modal Adjust

--- src/Components/Desktop/Forms.js
+++ src/Components/Desktop/Forms.js
@@ -10,21 +10,21 @@ const Forms = ({ handleClose }) => {
         className="-z-1 absolute inset-0 bg-[#00000050] cursor-pointer"
         onClick={() => handleClose()}
       ></div>
-      <div className="h-auto z-10 absolute md:left-1/2 md:-translate-x-1/2 mt-16 md:p-14 p-4 md:w-8/12 mx-5 rounded-md py-8  bg-white">
+      <div className="h-auto z-10 absolute md:left-1/2 md:-translate-x-1/2 mt-12 md:p-14 p-4 md:w-8/12 mx-5 rounded-md py-8  bg-white">
         <h1 className="text-center font-medium text-2xl">Unify Forms</h1>
 
-        <div className="grid md:grid-cols-2 divide-x divide-gray-300  mt-10 ">
+        <div className="grid md:grid-cols-2 divide-x divide-gray-300  mt-5 ">
           <div className="pr-[60px]">
             <h1 className="text-xl font-medium mb-7">
               For prospective students
             </h1>
-            <p className="mb-[60px]">
+            <p className="mb-[40px]">
               Looking to start your enrollment process? Find entry forms for all
               nursery, primary and secondary schools
             </p>
             <img
               src={findform}
-              className="h-[166px] w-[184px] mx-auto mb-16"
+              className="h-[166px] w-[184px] mx-auto mb-10"
               alt=""
             />
             <a
@@ -38,11 +38,11 @@ const Forms = ({ handleClose }) => {
             <h1 className="text-xl font-medium mb-7">
               For prospective students
             </h1>
-            <p className="mb-[60px]">
+            <p className="mb-[40px]">
               Looking to start your enrollment process? Find entry forms for all
               nursery, primary and secondary schools
             </p>
-            <img src={uploadform} className=" mx-auto mb-28" alt="" />
+            <img src={uploadform} className=" mx-auto mb-[90px]" alt="" />
             <a
               href="http://projectdemo.com.ng/colab/user/register"
               onClick={() => handleClose()}
