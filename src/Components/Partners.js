2b293d1a865c55d7bebc596c9bee996ca4ab272b
DevSaalih <soliualley@gmail.com>
2022-05-14 02:59:01 +0100

    Updates

--- src/Components/Partners.js
+++ src/Components/Partners.js
@@ -6,11 +6,11 @@ import partner2 from "../Assets/Svg/partner2.svg";
 import partner3 from "../Assets/Svg/partner3.svg";
 import partner4 from "../Assets/Svg/partner4.svg";
 import partner5 from "../Assets/Svg/partner5.svg";
-import Commentaries from "./Commentaries";
+// import Commentaries from "./Commentaries";
 
 const Partners = () => {
   return (
-    <div className=" md:-translate-y-20 transform bg-[#E7DFED] py-20">
+    <div className=" md:-translate-y-20 transform bg-[#E7DFED] pt-20 pb-5">
       <div className="px-10 text-center  text-[32px] text-[#1B1B22]">
         Driving
         <span className="relative mx-1">
@@ -30,9 +30,9 @@ const Partners = () => {
         <img src={partner5} className="h-[100px] w-[114px]" alt="" />
       </div>
 
-      <div className="text-center my-4"> <Commentaries /> </div>
-
-
+      {
+        // <div className="text-center my-4"> <Commentaries /> </div>
+      }
     </div>
   );
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -2,54 +2,46 @@ import React from "react";
 
 const about = () => {
   return (
-    <div className=" py-36 ">
+    <div className=" py-24 ">
       <div className="container w-11/12 mx-auto md:px-20 ">
-        <h1 className="text-[40px] font-medium mb-7">Enabling the future.</h1>
+        <h1 className="text-[40px] font-medium mb-7 text-center">About Us</h1>
 
         <p className="text-[#2F2D37] text-xl mb-7">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius proin
-          pellentesque nibh id nunc, enim, vestibulum consectetur. Nisl posuere
-          aliquam sodales lobortis nunc, tellus felis ac. Fusce mattis facilisis
-          est tortor ornare venenatis sagittis. Enim tellus adipiscing purus
-          suspendisse nulla tellus donec. Ut a est nisl.Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Varius proin pellentesque nibh id
-          nunc, enim, vestibulum consectetur. Nisl posuere aliquam sodales
-          lobortis nunc, tellus felis ac. Fusce mattis facilisis est tortor
-          ornare venenatis sagittis. Enim tellus adipiscing purus suspendisse
-          nulla tellus donec. Ut a est nisl.
+          C-One is a technology-driven company, incorporated with the primary
+          objective of providing start-ups with technology, business support,
+          and fund-raising opportunities to advance, accelerate and support
+          growth initiatives.
         </p>
-        <h1 className="text-[40px] font-medium mb-7">
-          Our mission is to help Nigerian universities ...
-        </h1>
-
-        <p className="text-[#2F2D37] text-xl mb-7">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius proin
-          pellentesque nibh id nunc, enim, vestibulum consectetur. Nisl posuere
-          aliquam sodales lobortis nunc, tellus felis ac. Fusce mattis facilisis
-          est tortor ornare venenatis sagittis. Enim tellus adipiscing purus
-          suspendisse nulla tellus donec. Ut a est nisl.Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Varius proin pellentesque nibh id
-          nunc, enim, vestibulum consectetur. Nisl posuere aliquam sodales
-          lobortis nunc, tellus felis ac. Fusce mattis facilisis est tortor
-          ornare venenatis sagittis. Enim tellus adipiscing purus suspendisse
-          nulla tellus donec. Ut a est nisl.
-        </p>
-        <h1 className="text-[40px] font-medium mb-7">
-          Our mission is to help Nigerian universities ...
-        </h1>
-
-        <p className="text-[#2F2D37] text-xl mb-7">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Varius proin
-          pellentesque nibh id nunc, enim, vestibulum consectetur. Nisl posuere
-          aliquam sodales lobortis nunc, tellus felis ac. Fusce mattis facilisis
-          est tortor ornare venenatis sagittis. Enim tellus adipiscing purus
-          suspendisse nulla tellus donec. Ut a est nisl.Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit. Varius proin pellentesque nibh id
-          nunc, enim, vestibulum consectetur. Nisl posuere aliquam sodales
-          lobortis nunc, tellus felis ac. Fusce mattis facilisis est tortor
-          ornare venenatis sagittis. Enim tellus adipiscing purus suspendisse
-          nulla tellus donec. Ut a est nisl.
-        </p>
+        <h1 className="text-[36px] font-medium mb-7">Mission</h1>
+        <div className="space-y-4">
+          <p>
+            To redefine business growth at all stages using its portfolio of
+            brands to provide bespoke solutions/services that focus on
+            community, technology, education, and investment.
+          </p>
+          <p>
+            To harness and combine the power of technology, community, and
+            investment to provide innovative solutions to emerging businesses.
+          </p>
+          <p>
+            To improve people’s lives through meaningful technological
+            innovation and support to help you make a difference.
+          </p>
+        </div>
+        <h1 className="text-[36px] font-medium my-7">Vision</h1>
+        <div className="space-y-4">
+          <p>
+            To reshape the future of businesses in all stages through
+            technological integration.
+          </p>
+          <p>
+            To provide exceptional technology solutions to every business idea.
+          </p>
+          <p>
+            To disrupt the start-up space by providing exceptional tech-driven
+            and community-oriented solutions to basic problems.
+          </p>
+        </div>
       </div>
     </div>
   );
